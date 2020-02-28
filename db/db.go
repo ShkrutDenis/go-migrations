141a612862853197ff356a3567343372c087fcef
InvolvedPanda <m.pechenin99@gmail.com>
2020-02-28 19:07:50 +0200

    add postgres connection  provider

--- db/db.go
+++ db/db.go
@@ -1,20 +1,43 @@
 package db
 
 import (
+	"errors"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
 	"os"
+	"strings"
 )
 
-func Connect() (*sqlx.DB, error) {
-	return sqlx.Connect(os.Getenv("DB_DRIVER"), getConnectionString())
+type connectionStringProvider func() string
+
+var connectionProvider map[string]connectionStringProvider = map[string]connectionStringProvider{
+	"mysql":    getMysqlConnectionString,
+	"postgres": getPostgresConnectionString,
 }
 
-func getConnectionString() string {
-	dbUser := os.Getenv("DB_USER")
-	dbPass := os.Getenv("DB_PASSWORD")
-	dbHost := os.Getenv("DB_CONNECTION")
-	dbName := os.Getenv("DB_NAME")
+func Connect() (*sqlx.DB, error) {
+	connectionStringProvider, ok := connectionProvider[os.Getenv("DB_DRIVER")]
+	if !ok {
+		return nil, errors.New("driver was not provided")
+	}
+
+	return sqlx.Connect(os.Getenv("DB_DRIVER"), connectionStringProvider())
+}
+
+func getMysqlConnectionString() string {
+	dbUser, dbPass, dbHost, dbName := os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_CONNECTION"), os.Getenv("DB_NAME")
 	return fmt.Sprintf("%v:%v@tcp(%v)/%v?parseTime=true", dbUser, dbPass, dbHost, dbName)
+}
+
+func getPostgresConnectionString() string {
+	connectionParts := strings.Split(":", os.Getenv("DB_CONNECTION"))
+	if len(connectionParts) < 2 {
+		return ""
+	}
+
+	dbHost, dbPort := connectionParts[0], connectionParts[1]
+	dbUser, dbPass, dbName := os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME")
+
+	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", dbHost, dbPort, dbUser, dbPass, dbName)
 }
