9809bad526ad608eac84de35c98dcb15aa6bf39d
Denis Shkrut <Shkrut_denis@rambler.ru>
2020-04-20 19:11:41 +0300

    Add builder for Postgres

---
+++ builder/postgress/column/column.go
@@ -0,0 +1,246 @@
+package column
+
+import (
+	"fmt"
+	"github.com/ShkrutDenis/go-migrations/builder/contract"
+	"github.com/ShkrutDenis/go-migrations/builder/postgress/info"
+	"github.com/jmoiron/sqlx"
+	"strings"
+)
+
+const (
+	null    = "NULL"
+	notNull = "NOT NULL"
+)
+
+type Column struct {
+	name      string
+	fieldType string
+
+	hasDefault   bool
+	defaultValue string
+
+	nullable      string
+	autoincrement bool
+
+	isPrimaryKey bool
+
+	unique       bool
+	hasUniqueKey bool
+
+	drop   bool
+	change bool
+	first  bool
+	after  string
+	rename string
+
+	info *info.ColumnInfo
+}
+
+func NewColumn(table, fieldName string, con *sqlx.DB) contract.Column {
+	ci := info.GetColumnInfo(table, fieldName, con)
+	c := &Column{name: fieldName, info: ci}
+	c.init()
+	return c
+}
+
+func (c *Column) init() contract.Column {
+	if c.info != nil {
+		c.fieldType = c.info.GetType()
+		if c.info.Nullable() {
+			c.nullable = null
+		} else {
+			c.nullable = notNull
+		}
+		c.unique = c.info.IsUnique()
+		c.hasUniqueKey = c.unique
+		c.hasDefault, c.defaultValue = c.info.HasDefault()
+		c.autoincrement = c.info.IsPrimary()
+	}
+	return c
+}
+
+// Functions for modify table
+func (c *Column) Type(fieldType string) contract.Column {
+	c.fieldType = fieldType
+	return c
+}
+
+func (c *Column) Nullable() contract.Column {
+	c.nullable = null
+	return c
+}
+
+func (c *Column) NotNull() contract.Column {
+	c.nullable = notNull
+	return c
+}
+
+func (c *Column) Autoincrement() contract.Column {
+	c.autoincrement = true
+	return c
+}
+
+func (c *Column) NotAutoincrement() contract.Column {
+	c.autoincrement = false
+	return c
+}
+
+func (c *Column) Default(value string) contract.Column {
+	c.hasDefault = value != ""
+	c.defaultValue = value
+	return c
+}
+
+func (c *Column) Primary() contract.Column {
+	c.isPrimaryKey = true
+	return c
+}
+
+func (c *Column) Unique() contract.Column {
+	c.unique = true
+	return c
+}
+
+func (c *Column) NotUnique() contract.Column {
+	c.unique = false
+	return c
+}
+
+func (c *Column) Drop() contract.Column {
+	c.drop = true
+	return c
+}
+
+func (c *Column) Change() contract.Column {
+	c.change = true
+	return c
+}
+
+func (c *Column) First() contract.Column {
+	c.first = true
+	return c
+}
+
+func (c *Column) After(name string) contract.Column {
+	c.after = name
+	return c
+}
+
+func (c *Column) Rename(name string) contract.Column {
+	c.rename = name
+	return c
+}
+
+// Functions for generate SQL
+func (c *Column) GetSQL() string {
+	if c.drop {
+		return c.dropColumnSQL()
+	}
+	if c.rename != "" {
+		return c.renameColumnSQL()
+	}
+	if c.change {
+		return c.changeColumnSQL()
+	}
+	return c.addColumnSQL()
+}
+
+func (c *Column) addColumnSQL() string {
+	return c.name + c.columnOptionsSQL() + c.columnPositionSQL()
+}
+
+func (c *Column) changeColumnSQL() string {
+	if c.info == nil {
+		panic(fmt.Sprintf("Column %v not found. Column name is correct?", c.name))
+	}
+	var sql string
+	sql += fmt.Sprintf("ALTER COLUMN %v DROP DEFAULT,", c.name)
+	if c.fieldType != c.info.GetType() {
+		sql += fmt.Sprintf("ALTER COLUMN %v TYPE %v USING %v::%v,", c.name, c.fieldType, c.name, c.fieldType)
+	}
+	sql += fmt.Sprintf("ALTER COLUMN %v SET DEFAULT '%v',", c.name, c.defaultValue)
+	if c.info.Nullable() && c.nullable == notNull {
+		sql += fmt.Sprintf("ALTER COLUMN %v SET NOT NULL,", c.name)
+	} else if !c.info.Nullable() && c.nullable == null {
+		sql += fmt.Sprintf("ALTER COLUMN %v DROP NOT NULL,", c.name)
+	}
+	return strings.TrimRight(sql, ",")
+}
+
+func (c *Column) renameColumnSQL() string {
+	return fmt.Sprintf("rename column %v to %v;", c.name, c.rename)
+}
+
+func (c *Column) dropColumnSQL() string {
+	return fmt.Sprintf("drop column %v,", c.name)
+}
+
+func (c *Column) columnOptionsSQL() string {
+	if c.isPrimaryKey {
+		return " serial primary key"
+	}
+	sql := " " + c.fieldType
+
+	if c.hasDefault {
+		sql += fmt.Sprintf(" default '%v'", c.defaultValue)
+	}
+	if c.nullable == "" {
+		c.nullable = notNull
+	}
+	sql += " " + c.nullable
+	return sql
+}
+
+func (c *Column) columnPositionSQL() string {
+	// TODO:Postgres don`t support positioning
+	return ""
+}
+
+// Helpful functions
+func (c *Column) GetName() string {
+	return c.name
+}
+
+func (c *Column) GetUniqueKeyName() string {
+	if c.info == nil {
+		return ""
+	}
+	k := c.info.GetUniqueKey()
+	if k == nil {
+		return ""
+	}
+	return k.KeyName
+}
+
+func (c *Column) IsPrimary() bool {
+	return c.isPrimaryKey
+}
+
+func (c *Column) IsUnique() bool {
+	return c.unique
+}
+
+func (c *Column) HasUniqueKey() bool {
+	return c.hasUniqueKey
+}
+
+func (c *Column) NeedUniqueKey() bool {
+	return c.unique && !c.hasUniqueKey
+}
+
+func (c *Column) NeedDropUniqueKey() bool {
+	return !c.unique && c.hasUniqueKey
+}
+
+func (c *Column) IsWaitingDrop() bool {
+	return c.drop
+}
+
+func (c *Column) IsWaitingRename() bool {
+	return c.rename != ""
+}
+
+func (c *Column) IsWaitingChange() bool {
+	return c.change
+}
---
+++ builder/postgress/table/table.go
@@ -0,0 +1,270 @@
+package table
+
+import (
+	"fmt"
+	"github.com/ShkrutDenis/go-migrations/builder/contract"
+	"github.com/ShkrutDenis/go-migrations/builder/postgress/column"
+	"github.com/ShkrutDenis/go-migrations/builder/postgress/info"
+	"github.com/ShkrutDenis/go-migrations/builder/postgress/key"
+	"github.com/jmoiron/sqlx"
+	"log"
+	"strconv"
+	"strings"
+)
+
+type Table struct {
+	name    string
+	newName string
+
+	foreignKeys []contract.ForeignKey
+	uniqueKeys  []contract.UniqueKey
+
+	columns    []contract.Column
+	timestamps bool
+
+	drop   bool
+	change bool
+
+	connect *sqlx.DB
+}
+
+func NewTable(name string, con *sqlx.DB) contract.Table {
+	return &Table{name: name, connect: con.Unsafe()}
+}
+
+func DropTable(name string, con *sqlx.DB) contract.Table {
+	return &Table{name: name, drop: true, connect: con.Unsafe()}
+}
+
+func ChangeTable(name string, con *sqlx.DB) contract.Table {
+	return &Table{name: name, change: true, connect: con.Unsafe()}
+}
+
+func RenameTable(oldName, newName string, con *sqlx.DB) contract.Table {
+	return &Table{name: oldName, newName: newName, connect: con.Unsafe()}
+}
+
+// Functions for table columns
+func (t *Table) Column(name string) contract.Column {
+	c := column.NewColumn(t.name, name, t.connect)
+	t.columns = append(t.columns, c)
+	return c
+}
+
+func (t *Table) String(name string, length int) contract.Column {
+	if length < 0 {
+		length = 255
+	}
+	c := t.Column(name).Type("varchar(" + strconv.Itoa(length) + ")")
+	return c
+}
+
+func (t *Table) Integer(name string) contract.Column {
+	c := t.Column(name).Type("int")
+	return c
+}
+
+func (t *Table) WithTimestamps() contract.Table {
+	t.timestamps = true
+	return t
+}
+
+func (t *Table) getTimeStampsSQL() string {
+	return "created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP," +
+		"updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP"
+}
+
+func (t *Table) RenameColumn(oldName, newName string) contract.Column {
+	c := t.Column(oldName).Rename(newName)
+	return c
+}
+
+func (t *Table) DropColumn(name string) contract.Column {
+	c := t.Column(name).Drop()
+	return c
+}
+
+// Functions for keys
+func (t *Table) PrimaryKey(Column string) contract.Column {
+	if ok, c := t.hasColumn(Column); ok {
+		return c.Primary()
+	} else {
+		return t.Integer("id").Primary()
+	}
+}
+
+func (t *Table) ForeignKey(Column string) contract.ForeignKey {
+	k := key.NewForeignKey(t.name, Column)
+	t.foreignKeys = append(t.foreignKeys, k)
+	return k
+}
+
+func (t *Table) DropForeignKey(name string) contract.Table {
+	if !t.change {
+		return t
+	}
+	ki := info.GetKeyInfo(name, t.connect)
+	if ki == nil {
+		panic("Foreign key " + name + " not exist. Foreign key name is correct?")
+	}
+	t.foreignKeys = append(t.foreignKeys, key.NewForeignKeyByKeyInfo(ki).Drop())
+	return t
+}
+
+// Generate SQL functions
+func (t *Table) GetSQL() string {
+	if t.drop {
+		return t.dropTableSQL()
+	}
+	if t.newName != "" {
+		return t.renameTableSQL()
+	}
+	sql := ""
+	for _, k := range t.foreignKeys {
+		if k.ForDrop() {
+			sql += k.GetSQL()
+		}
+	}
+	if t.change {
+		return sql + t.changeTableSQL()
+	}
+	return sql + t.createTableSQL()
+}
+
+func (t *Table) createTableSQL() string {
+	sql := "CREATE TABLE " + t.name + "("
+	for _, c := range t.columns {
+		sql += c.GetSQL() + ","
+		t.checkUniqueKey(c)
+	}
+	if t.timestamps {
+		sql += t.getTimeStampsSQL() + ","
+	}
+	for _, k := range t.foreignKeys {
+		if k.ForDrop() {
+			continue
+		}
+		if k.GetName() == "" {
+			k.GenerateKeyName()
+		}
+		sql += k.GetSQL() + ","
+	}
+	return strings.TrimRight(sql, ",") + ");"
+}
+
+func (t *Table) changeTableSQL() string {
+	base := "ALTER TABLE " + t.name + " "
+	var forAdd string
+	var forModify string
+	var forRename string
+	var forDrop string
+	var fKeys string
+	for _, c := range t.columns {
+		if c.IsWaitingDrop() {
+			forDrop += c.GetSQL()
+			continue
+		}
+		t.checkUniqueKey(c)
+		if c.IsWaitingRename() {
+			forRename += c.GetSQL()
+			continue
+		}
+		if c.IsWaitingChange() {
+			forModify += c.GetSQL() + ","
+			continue
+		}
+		forAdd += "add " + c.GetSQL() + ","
+	}
+	for _, k := range t.foreignKeys {
+		if k.ForDrop() {
+			continue
+		}
+		if k.GetName() == "" {
+			k.GenerateKeyName()
+		}
+		fKeys += "add " + k.GetSQL() + ","
+	}
+	if forDrop != "" {
+		forDrop = base + strings.TrimRight(forDrop, ",") + ";"
+	}
+	if forRename != "" {
+		forRename = base + forRename + ";"
+	}
+	if forModify != "" {
+		forModify = base + strings.TrimRight(forModify, ",") + ";"
+	}
+	if forAdd != "" {
+		forAdd = base + strings.TrimRight(forAdd, ",") + ";"
+	}
+	if fKeys != "" {
+		fKeys = base + strings.TrimRight(fKeys, ",") + ";"
+	}
+	return forRename + forDrop + forModify + forAdd + fKeys
+}
+
+func (t *Table) dropTableSQL() string {
+	return fmt.Sprintf("DROP TABLE %v;", t.name)
+}
+
+func (t *Table) renameTableSQL() string {
+	return fmt.Sprintf("ALTER TABLE %v RENAME TO %v;", t.name, t.newName)
+}
+
+// Execution functions
+func (t *Table) Exec() error {
+	queries := strings.Split(t.GetSQL(), ";")
+	for i, q := range queries {
+		if i == len(queries) {
+			break
+		}
+		_, err := t.connect.Exec(q + ";")
+		if err != nil {
+			return err
+		}
+	}
+
+	for _, k := range t.uniqueKeys {
+		if err := k.Exec(t.connect); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (t *Table) MustExec() {
+	queries := strings.Split(t.GetSQL(), ";")
+	for _, q := range queries {
+		if q == "" {
+			break
+		}
+		log.Println(q)
+		t.connect.MustExec(q + ";")
+	}
+
+	for _, k := range t.uniqueKeys {
+		k.MustExec(t.connect)
+	}
+}
+
+// Helpful functions
+func (t *Table) checkUniqueKey(c contract.Column) {
+	if c.NeedUniqueKey() {
+		k := key.NewUniqueKey(t.name, c.GetName()).GenerateKeyName()
+		t.uniqueKeys = append(t.uniqueKeys, k)
+	}
+	if c.NeedDropUniqueKey() {
+		k := key.NewUniqueKey(t.name, c.GetName()).SetKeyName(c.GetUniqueKeyName()).Drop()
+		t.uniqueKeys = append(t.uniqueKeys, k)
+	}
+}
+
+// Helpful functions
+func (t *Table) hasColumn(name string) (bool, contract.Column) {
+	for _, c := range t.columns {
+		if c.GetName() == name {
+			return true, c
+		}
+	}
+	return false, nil
+}
